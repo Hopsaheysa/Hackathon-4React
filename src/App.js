c5ae71b0bb0762cb12a60554737cc2080e7cceef
Hopsaheysa <stranskymarek90@gmail.com>
2021-06-25 10:42:46 +0200

    with fetching

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+import { DateTime } from 'luxon';
+
 
 function App() {
+  const [flights, setFlights] = useState(null);
+  const [destinationFrom, setDestinationFrom] = useState("PRG");
+  const [destinationTo, setDestinationTo] = useState("VIE");
+  const [dateDeparture, setDateDeparture] = useState("25/06/2021");
+  const [dateArrival, setDateArrival] = useState("30/06/2021");
+
+
+  async function fetchFlights() {    
+
+    const resp = await fetch(`https://api.skypicker.com/flights?fly_from=${destinationFrom}&fly_to=${destinationTo}&date_from=${dateDeparture}&date_to=${dateArrival}&partner=data4youcbp202106`);
+    const flights = await resp.json();
+    console.log(flights);
+
+    return setFlights(flights);
+
+  }
+
+  useEffect(() => {
+    fetchFlights()
+  }, [])
+
+  if (flights === null) return "Loading the flights";
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>Hi</>
+  )
+
 }
 
 export default App;
